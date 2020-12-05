a9e984abd82de343e563f9fb216898cbda392319
Geovani José <geovani-lol@hotmail.com>
2020-12-04 23:48:48 -0300

    Add useCart hook on FloatingCart component

--- src/components/FloatingCart/index.tsx
+++ src/components/FloatingCart/index.tsx
@@ -1,5 +1,8 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { useNavigation } from '@react-navigation/native';
+
+import { useCart } from '../../hooks/cart';
+import formatValue from '../../utils/formatValue';
 
 import CartIcon from '../../assets/cart-icon.svg';
 
@@ -13,7 +16,27 @@ import {
 } from './styles';
 
 const FloatingCart: React.FC = () => {
+  const { products } = useCart();
+
   const navigation = useNavigation();
+
+  const cartTotal = useMemo(() => {
+    const total = products.reduce(
+      (accumulator, product) => accumulator + product.quantity * product.price,
+      0,
+    );
+
+    return formatValue(total);
+  }, [products]);
+
+  const totalItensInCart = useMemo(() => {
+    const totalItens = products.reduce(
+      (accumulator, product) => accumulator + product.quantity,
+      0,
+    );
+
+    return totalItens;
+  }, [products]);
 
   return (
     <>
@@ -22,11 +45,11 @@ const FloatingCart: React.FC = () => {
       <Container>
         <CartButton onPress={() => navigation.navigate('Cart')}>
           <CartIcon width={24} height={24} color="#fff" />
-          <CartButtonText>24 itens</CartButtonText>
+          <CartButtonText>{`${totalItensInCart} itens`}</CartButtonText>
         </CartButton>
 
         <CartPricing>
-          <CartTotalPrice>R$15.000,00</CartTotalPrice>
+          <CartTotalPrice>{cartTotal}</CartTotalPrice>
         </CartPricing>
       </Container>
     </>
